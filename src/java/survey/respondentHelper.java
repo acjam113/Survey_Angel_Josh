c40316ef715837fb74e227c884c04a06c139cc0d
acjam113 <acjam113@gmail.com>
2017-03-22 22:36:47 -0700

    Updated version of respondent file

--- src/java/survey/respondentHelper.java
+++ src/java/survey/respondentHelper.java
@@ -1,8 +1,6 @@
 
 package survey;
 
-import com.sun.corba.se.impl.logging.ORBUtilSystemException;
-import com.sun.media.sound.DLSModulator;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SQLQuery;
@@ -24,14 +22,108 @@ public class respondentHelper {
             e.printStackTrace();
         }
     }
+   
+    public int getSurveyId(){
+        
+        List<Survey> surveyList = null;
+        
+        String sql = "select * from survey order by Survey_ID desc limit 1";
+        
+        try{
+             
+         // if this transaction is not active, make it active
+            if(!this.session.getTransaction().isActive()){
+                session.beginTransaction();
+            }
+            
+            // creating actual query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+            
+            // associating the actor table and the actor POJO
+            q.addEntity(Survey.class);
+            
+            // executes the query and returns it as a list
+            surveyList = (List<Survey>)q.list(); 
+                       
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return surveyList.get(0).getSurveyId();
+    }
+    
+    public int getRespondentId(){
+        
+        List<Respondent> respondentList = null;
+        
+        String sql = "select Respondent_ID from respondent order by Respondent_ID desc limit 1";
+        
+        try{
+             
+         // if this transaction is not active, make it active
+            if(!this.session.getTransaction().isActive()){
+                session.beginTransaction();
+            }
+            
+            // creating actual query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+            
+            // associating the actor table and the actor POJO
+            q.addEntity(User.class);
+            
+            // executes the query and returns it as a list
+            respondentList = (List<Respondent>)q.list();
+            
+                       
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return respondentList.get(0).getRespondentId();
+    }
+    
+    private int insertSurveyRespondent(){
+        
+        int result = 0;
+        
+        String sql = "insert values (:surveyId, :respondentId)";
+        
+        try{
+            // checks to see if the transaction is active
+            if(!this.session.getTransaction().isActive()){   
+                session.beginTransaction();
+            }
+            
+            // creating a query that can be executed
+            SQLQuery q = session.createSQLQuery(sql);
+            
+            // associating User POJO and table with a query
+            q.addEntity(SurveyRespondent.class);
+            
+            // binds values to the placeholders in the query
+            q.setParameter("surveyId", getSurveyId());
+            q.setParameter("respondentId", getRespondentId());
+           
+            // executes the query
+            result = q.executeUpdate();
+            
+            // commits the query to the database
+            session.getTransaction().commit();
+            
+            
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
     
     public int insertRespondent(Respondent a){
         
         int result = 0;
             
-            
             String sql = "insert into respondent (Respondent_Email) "
-                + "value (:respondent)";
+                + "values (:respondentEmail)";
         
         try{
             // checks to see if the transaction is active
@@ -47,7 +139,8 @@ public class respondentHelper {
             
                 
                  // binds values to the placeholders in the query
-                 q.setParameter("respondent", a.getRespondentEmail());
+                 //q.setParameter("respondentId", a.getRespondentId());
+                 q.setParameter("respondentEmail", a.getRespondentEmail());
            
                 // executes the query
                 result = q.executeUpdate();
@@ -55,8 +148,11 @@ public class respondentHelper {
                 // commits the query to the database
                 session.getTransaction().commit();
             }catch (Exception e){
-            e.printStackTrace();
-            }  
+                e.printStackTrace();
+                
+            }
+        
+        insertSurveyRespondent();
         return result;
     }  
 }
